3bf34dad943ac4d28f5fea76b40010f973f17105
Czn <2540719857@qq.com>
2026-03-16 17:06:28 +0800

    feat: init back

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { DialectModule } from './dialect/dialect.module';
+
+import { AuthModule } from './common/module/auth/auth.module';
+import { RedisUseModule } from './common/module/redis/redis.module'; // 导入 Redis 模块
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'caixin2580', // 请替换为你的MySQL密码
+      database: 'langauge_platform', // 请替换为你的数据库名
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // 开发环境可以设为true，生产环境建议设为false
+    }),
+    RedisUseModule, // 全局 Redis 模块（关键）
+    // 2. 公共认证模块
+    AuthModule,
+    UserModule,
+    DialectModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
